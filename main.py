924404f1e45b9c99d7d0feb2f40f73248ee69a08
YashDevdhe <yashdevdhe05@gmail.com>
2026-06-11 10:49:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def calculate_marks(marks):
+    highest = max(marks)
+    lowest = min(marks)
+    average = sum(marks) / len(marks)
+
+    print("Marks:", marks)
+    print("Highest Marks:", highest)
+    print("Lowest Marks:", lowest)
+    print("Average Marks:", average)
+
+
+if __name__ == "__main__":
+    marks = [85, 92, 78, 88, 95]
+    calculate_marks(marks)
